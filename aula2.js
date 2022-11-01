9f396b488e834a8cf114421ddf366df6e424de4d
Luan Cunha <95319515+lluancunha@users.noreply.github.com>
2022-11-01 19:07:26 -0300

    Create aula2.js

---
+++ aula2.js
@@ -0,0 +1,19 @@
+/**
+ * Operadores
+ */
+
+console.log ("Operadores Aritméticos")
+console.log ("+ | soma")
+console.log ("- | subtração")
+console.log ("* | multiplicação")
+console.log ("/ | divisão")
+console.log ("% | modulo (resto da divisão)")
+console.log("Exemplos:")
+let x = 10
+console.log("x = " + x)
+console.log ("x + 5 = " + (x + 5))
+console.log ("x - 5 = "+ (x - 5))
+console.log ("x * 5 = " + (x * 5))
+console.log ("x / 5 = " + (x / 5))
+console.log ("x % 5 = " + (x % 5) + " (Sobra da Divisão)")
+console.log ("_________________________________________")
