b09c1b8abafc04c152580cf8265a8bae65e9c845
Luan Cunha <95319515+lluancunha@users.noreply.github.com>
2022-11-01 19:08:17 -0300

    Create aula3.js

---
+++ aula3.js
@@ -0,0 +1,21 @@
+/**
+ * Operadores
+ */
+
+ console.log ("Operadores de Atribuição")
+ console.log ("+= | soma e atribuição a variável")
+ console.log ("-= | subtração e atribuição a variável")
+ console.log ("*= | multiplicação e atribuição a variável")
+ console.log ("/= | divisão e atribuição a variável")
+ console.log ("++ | soma 1 ao resultado")
+ console.log ("-- | subtrai 1 ao resultado")
+ console.log("Exemplos:")
+ let x = 10
+ console.log("x = " + x)
+ console.log ("10 += 5 = " + (x += 5))
+ console.log ("15 -= 5 = "+ (x -= 5))
+ console.log ("10 *= 5 = " + (x *= 5))
+ console.log ("50 /= 5 = " + (x /= 5))
+ console.log (x++)
+ console.log (x--)
+ console.log ("_________________________________________")
